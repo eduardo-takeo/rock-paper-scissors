994d9e9882755ea8b26f733d0fb4747dee193698
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-06-08 23:30:12 -0300

    Add startGame function

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,27 @@
-const options = {
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+const plays = {
   1: "Rock",
   2: "Paper",
   3: "Scissors",
 };
 
-function randomizeChoice() {
-  return Math.floor(Math.random() * 3) + 1;
+//TODO
+rockButton.addEventListener("click", startGame);
+paperButton.addEventListener("click", startGame);
+scissorsButton.addEventListener("click", startGame);
+
+function startGame(humanPlay) {
+  const cpuPlay = Math.floor(Math.random() * 3) + 1;
+
+  switch (humanPlay) {
+    case 1:
+      return cpuPlay === 3 ? "Winner" : "Loser";
+    case 2:
+      return cpuPlay === 1 ? "Winner" : "Loser";
+    case 3:
+      return cpuPlay === 2 ? "Winner" : "Loser";
+  }
 }
