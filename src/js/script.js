b9579d4959482c169095a537a98950cf897e6ea1
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-06-10 22:29:17 -0300

    Add comments to separate script elements

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,22 @@
+//Elements
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 const score = document.getElementById("score");
 
+//Enum
 const plays = {
   1: "Rock",
   2: "Paper",
   3: "Scissors",
 };
 
+//Events
 rockButton.addEventListener("click", () => startGame(1));
 paperButton.addEventListener("click", () => startGame(2));
 scissorsButton.addEventListener("click", () => startGame(3));
 
+//Functions
 function startGame(playerChoice) {
   let result = "";
   const cpuChoice = Math.floor(Math.random() * 3) + 1;
@@ -32,6 +36,7 @@ function startGame(playerChoice) {
       break;
   }
 
+  updateScore(result);
   showResult(result, playerChoice, cpuChoice);
 }
 
@@ -40,8 +45,6 @@ function showResult(result, playerChoice, cpuChoice) {
   alert(
     `${result} \n Player chose ${plays[playerChoice]} and CPU chose ${plays[cpuChoice]}`
   );
-
-  updateScore(result);
 }
 
 function updateScore(result) {
